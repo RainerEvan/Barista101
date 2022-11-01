fee0235a87eae5e1614016af2141fe7ee1a34ecb
RainerEvan <rainerevan@gmail.com>
2022-11-01 21:34:31 +0700

    menambahkan entity

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,27 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path:'',
-  //   redirectTo: 'dashboard',
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path:'login', 
-  //   component: ,
-  // },
-  // {
-  //   path:'', 
-  //   component: , 
-  //   canActivate:,
-  //   children:[
-  //     {
-  //       path:'dashboard', 
-  //       component: ,
-  //       canActivate:
-  //     },
-  //   ]
-  // }
 ];
 
 @NgModule({
